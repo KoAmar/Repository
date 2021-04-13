199428b0ae30881e2ecf7982d8d02f1c69f5255c
KoAmar <43239620+KoAmar@users.noreply.github.com>
2021-04-13 14:21:04 +0300

    email confirmation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Repository.Models;
+using Repository.Models.Business;
 using Repository.ViewModels;
 
 namespace Repository.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
         public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
@@ -27,19 +31,58 @@ namespace Repository.Controllers
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
             if (!ModelState.IsValid) return View(model);
-            var user = new User {Email = model.Email, UserName = model.Email, Year = model.Year};
+            var user = new User {Email = model.Email, UserName = model.Email};
             // добавляем пользователя
             var result = await _userManager.CreateAsync(user, model.Password);
-
             if (result.Succeeded)
             {
-                // установка куки
-                await _signInManager.SignInAsync(user, false);
-                return RedirectToAction("Index", "Home");
+                // генерация токена для пользователя
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var callbackUrl = Url.Action(
+                    "ConfirmEmail",
+                    "Account",
+                    new {userId = user.Id, code = code},
+                    protocol: HttpContext.Request.Scheme);
+                var emailService = new EmailService();
+                await emailService.SendEmailAsync(model.Email, "Confirm your account",
+                    $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
+                 // emailService.SendEmailAsync(model.Email,"User", "Confirm your account",
+                 //    $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
+
+                return Content(
+                    "Для завершения регистрации проверьте электронную почту и перейдите по ссылке, указанной в письме");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
 
-            foreach (var error in result.Errors) ModelState.AddModelError(string.Empty, error.Description);
             return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return View("Error");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if (result.Succeeded)
+                return RedirectToAction("Index", "Home");
+            else
+                return View("Error");
         }
 
         [HttpGet]
@@ -53,28 +96,41 @@ namespace Repository.Controllers
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (!ModelState.IsValid) return View(model);
-            var result =
-                await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
-            if (result.Succeeded)
+            var user = await _userManager.FindByNameAsync(model.Email);
+            if (user != null)
             {
-                // проверяем, принадлежит ли URL приложению
-                if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
-                    return Redirect(model.ReturnUrl);
-                return RedirectToAction("Index", "Home");
+                // проверяем, подтвержден ли email
+                if (!await _userManager.IsEmailConfirmedAsync(user))
+                {
+                    ModelState.AddModelError(string.Empty, "Вы не подтвердили свой email");
+                    return View(model);
+                }
             }
 
-            ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+            var result =
+                await _signInManager.PasswordSignInAsync
+                    (model.Email, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+            }
 
             return View(model);
         }
 
-        [HttpGet]
+        // [HttpGet]
         // [ValidateAntiForgeryToken]
         public async Task<IActionResult> Logout()
         {
-            // удаляем аутентификационные куки
+            // await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             await _signInManager.SignOutAsync();
+            // HttpContext.Response.Cookies.Delete(".AspNetCore.Cookies");
             return RedirectToAction("Index", "Home");
+            // RedirectToAction();
         }
     }
 }
---
+++ Models/Business/EmailService.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using MimeKit;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+// using System.Net;
+// using System.Net.Mail;
+
+namespace Repository.Models.Business
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+        
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "spam.corp.asp@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+        
+            using var client = new SmtpClient();
+            await client.ConnectAsync("smtp.gmail.com", 465, true);
+            await client.AuthenticateAsync("spam.corp.asp@gmail.com", "56MBNx76DhJEdgZ");
+            await client.SendAsync(emailMessage);
+            await client.SendAsync(emailMessage);
+        
+            await client.DisconnectAsync(true);
+        }
+        // public bool Send(string receiverEmail, string receiverName, string subject, string body)
+        // {
+        //     var mailMessage = new MailMessage();
+        //     var mailAddress = new MailAddress
+        //         ("spam.corp.asp@gmail.com", "Sender Name"); // abc@gmail.com = input Sender Email Address 
+        //     mailMessage.From = mailAddress;
+        //     mailAddress = new MailAddress(receiverEmail, receiverName);
+        //     mailMessage.To.Add(mailAddress);
+        //     mailMessage.Subject = subject;
+        //     mailMessage.Body = body;
+        //     mailMessage.IsBodyHtml = true;
+        //
+        //     var mailSender = new SmtpClient("smtp.gmail.com", 587)
+        //     {
+        //         EnableSsl = true,
+        //         UseDefaultCredentials = false,
+        //         DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network,
+        //         Credentials = new NetworkCredential
+        //             ("spam.corp.asp@gmail.com", "56MBNx76DhJEdgZ")   // abc@gmail.com = input sender email address  
+        //         //pass = sender email password
+        //     };
+        //
+        //     try
+        //     {
+        //         mailSender.Send(mailMessage);
+        //         return true;
+        //     }
+        //     catch (SmtpFailedRecipientException ex)
+        //     { 
+        //         // Write the exception to a Log file.
+        //     }
+        //     catch (SmtpException ex)
+        //     { 
+        //         // Write the exception to a Log file.
+        //     }
+        //     finally
+        //     {
+        //         mailSender = null;
+        //         mailMessage.Dispose();
+        //     }
+        //     return false;
+        // }
+    }
+}
