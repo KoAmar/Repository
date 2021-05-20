2555d2a3444b3a24e23673bd287e82b248bd2865
KoAmar <43239620+KoAmar@users.noreply.github.com>
2021-05-20 19:57:48 +0300

    file deleting from project

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -59,12 +59,12 @@ namespace Repository.Controllers
         {
             var file = _context.Files.FindAsync(fileId).Result;
             if (file == null) return NotFound();
-            
+
             var fileProvider = new FileExtensionContentTypeProvider();
-            
+
             if (!fileProvider.TryGetContentType(file.Name, out string contentType))
             {
-                View("Message",$"Unable to find Content Type for file name {file.Name}.");
+                View("Message", $"Unable to find Content Type for file name {file.Name}.");
             }
 
             return DownloadFile(fileId);
@@ -74,13 +74,36 @@ namespace Repository.Controllers
         {
             var file = _context.Files.FindAsync(fileId).Result;
             var fileProvider = new FileExtensionContentTypeProvider();
-            
+
             if (!fileProvider.TryGetContentType(file.Name, out string contentType))
             {
                 throw new ArgumentOutOfRangeException($"Unable to find Content Type for file name {file.Name}.");
             }
 
             return File(file.FilePath, contentType, file.Name);
+        }
+
+        public IActionResult DeleteFile(string fileId)
+        {
+            var file = _context.Files.FindAsync(fileId).Result;
+            if (file == null) return NotFound();
+
+            var fullFilePath = Path.Combine(_appEnvironment.WebRootPath, file.FilePath);
+
+            if (System.IO.File.Exists(fullFilePath))
+            {
+                System.IO.File.Delete(fullFilePath);
+            }
+
+            var projectId = file.ProjectId;
+            var project = _context.CourseProjects.FindAsync(projectId).Result;
+
+            _context.Files.Remove(file);
+            _context.SaveChangesAsync();
+
+            if (project == null) return View("Message", "Проект, привязанный к файлу отсуствует");
+
+            return RedirectToAction("EditProject", "Projects", new {id = projectId});
         }
     }
 }
